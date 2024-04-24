c3fa2744d9eeb61373e3d5dfd289bc31a2e8ccae
Hellerson Murilo B Calabianqui de Souza - Libbs <hellerson.souza@libbs.com.br>
2024-04-24 20:56:07 -0300

    adição do controller DELETAR -> Produtos

--- src/controller/produtosController.js
+++ src/controller/produtosController.js
@@ -27,6 +27,44 @@ class ProdutosController {
             });
         }
     }
+
+    async deletar(req, res) {
+        try {
+            //capturar o id que é para ser deletado
+            const { id } = req.params;
+
+            //verificar se o id foi informado
+            if (!id) {
+                return res.status(400).json({
+                    error: 'Id não informado!'
+                });
+            }
+
+            //excluindo o produto com base no id
+            const excluirProduto = await produtoModel.destroy({
+                where: {
+                    id: id
+                }
+            })
+            //verificar se o produto foi excluido
+            if (!excluirProduto) {
+                return res.status(400).json({
+                    error: 'Produto não encontrado'
+                })
+            }
+            
+            //SUCESSO
+            return res.status(200).json({
+                mensagem: 'Produto excluído com sucesso!'
+            })
+
+        } catch (error) {
+            return res.status(400).json({
+                msg: 'Produtos não excluido',
+                err: error
+            })
+        }
+    }
 }
 
 module.exports = ProdutosController;
--- src/routes/produtos.Routes.js
+++ src/routes/produtos.Routes.js
@@ -1,14 +1,12 @@
 const { Router } = require('express')
-
 const produtosRouter = Router()
 
 const ProdutosController = require('../controller/produtosController')
-
 const produtoController = new ProdutosController()
 
 produtosRouter.get('/listar')
 
-produtosRouter.delete('/deletar/:id')
+produtosRouter.delete('/deletar/:id', produtoController.deletar)
 
 produtosRouter.put('/atualizar/:id')
 
